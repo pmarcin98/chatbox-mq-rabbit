9321fc9be4adf826b106b41d0591e476ebf70251
Piotr <bujan98pl@gmail.com>
2022-01-08 17:14:25 +0100

    Add base

---
+++ src/test/java/com/vistula/chatboxrabbimq/ChatboxRabbimqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vistula.chatboxrabbimq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatboxRabbimqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
