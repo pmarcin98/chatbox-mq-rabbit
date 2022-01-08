eac2fe125dc04250f1474fce4fe880db3a874307
Piotr <bujan98pl@gmail.com>
2022-01-08 20:47:19 +0100

    Add RabbitListener

--- chatbox-rabbitmq-consument/src/main/java/com/vistula/chatboxrabbitmqconsument/ClientMessage.java
+++ chatbox-rabbitmq-consument/src/main/java/com/vistula/chatboxrabbitmqconsument/ClientMessage.java
@@ -1,10 +1,9 @@
 package com.vistula.chatboxrabbitmqconsument;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +13,13 @@ public class ClientMessage {
     private RabbitTemplate rabbitTemplate;
 
     @GetMapping("/receiveMessage")
-    public String get() {
+    public Object get() {
         Object message = rabbitTemplate.receiveAndConvert( "kolejka");
-        return message.toString();
+        return message;
+    }
+
+    @RabbitListener(queues = "kolejka")
+    public void get(String s) {
+        System.out.println(s);
     }
 }
